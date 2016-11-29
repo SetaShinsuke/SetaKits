b5a67b59bcd819427a4ce7e5b3339224609d2bf5
Seta <sweater@azusasoft.com>
2016-11-29 19:27:31 +0800

    useless

---
+++ setakits/src/main/java/com/seta/setakits/StringHelper.java
@@ -0,0 +1,22 @@
+package com.seta.setakits;
+
+/**
+ * Created by SETA_WORK on 2016/11/29.
+ */
+
+public class StringHelper {
+
+//    public static String getContent(EditText editText){
+//        if(editText.getText() == null || editText.getText().length()==0){
+//            return null;
+//        }
+//        return editText.getText().toString();
+//    }
+//
+//    public static String getContent( editText){
+//        if(editText.getText() == null || editText.getText().length()==0){
+//            return null;
+//        }
+//        return editText.getText().toString();
+//    }
+}
